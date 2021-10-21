b30617cc2fdfafafb8a683230ec27755891404b2
Manfred Gonzalez-Hernandez <gonzalezhernandez.manfred@gmail.com>
2021-10-21 05:55:48 +0200

    Update model.py

--- model.py
+++ model.py
@@ -1,6 +1,8 @@
 from typing import ForwardRef
 from numpy.core.numeric import identity
 import torch
+from torch.nn.modules.activation import LeakyReLU
+from torch.nn.modules.batchnorm import BatchNorm2d
 import torchvision
 import torchvision.transforms as transforms
 import torch.nn as nn
@@ -17,18 +19,13 @@ def get_paddingSizes(x, layer):
 
 
 class Model(nn.Module):
-    def __init__(self,epsilon,sensitivity):
+    def __init__(self,epsilon=0,sensitivity=1):
         super(Model, self).__init__()
 
         #self.leaky_Relu = nn.LeakyReLU(negative_slope=0.3)
         self.epsilon = epsilon
         self.sensitivity = sensitivity
         self.upsamplig2D = nn.UpsamplingNearest2d(scale_factor=2)
-        #self.maxPool2D = nn.MaxPool2d([1,1])
-        #self.sigmoid = nn.Sigmoid()
-        self.batchnorm128 = nn.BatchNorm2d(128)
-        self.batchnorm96 = nn.BatchNorm2d(96)
-        self.batchnorm32 = nn.BatchNorm2d(32)
 
         self.fc1_id = nn.Linear(in_features=67, out_features=512) # for identity input
         self.fc1_ori = nn.Linear(in_features=2, out_features=512) # for orientation input
@@ -37,25 +34,51 @@ class Model(nn.Module):
         self.fc2 = nn.Linear(in_features=1536, out_features=1024) # for the concatenated 3 inputs layer
         self.fc3 = nn.Linear(in_features=1024, out_features=5*4*128) # the final linear layer before the upsampling
         
-        self.conv1 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(5,5))
-        self.conv1_2 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3,3))
+        self.upconv1 = nn.Sequential(
+            nn.UpsamplingNearest2d(scale_factor=2),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(5,5), padding='same'),
+            nn.LeakyReLU(True),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3,3), padding='same'),
+            nn.LeakyReLU(True),
+            nn.BatchNorm2d(128)
+        )
+        self.upconv2 = nn.Sequential(
+            nn.UpsamplingNearest2d(scale_factor=2),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(5,5), padding='same'),
+            nn.LeakyReLU(True),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3,3), padding='same'),
+            nn.LeakyReLU(True),
+            nn.BatchNorm2d(128)
+        )
+        self.upconv3 = nn.Sequential(
+            nn.UpsamplingNearest2d(scale_factor=2),
+            nn.Conv2d(in_channels=128, out_channels=96, kernel_size=(5,5), padding='same'),
+            nn.LeakyReLU(True),
+            nn.Conv2d(in_channels=96, out_channels=96, kernel_size=(3,3), padding='same'),
+            nn.LeakyReLU(True),
+            nn.BatchNorm2d(96)
+        )
+        self.upconv4 = nn.Sequential(
+            nn.UpsamplingNearest2d(scale_factor=2),
+            nn.Conv2d(in_channels=96, out_channels=96, kernel_size=(5,5), padding='same'),
+            nn.LeakyReLU(True),
+            nn.Conv2d(in_channels=96, out_channels=96, kernel_size=(3,3), padding='same'),
+            nn.LeakyReLU(True),
+            nn.BatchNorm2d(96)
+        )
+        self.upconv5 = nn.Sequential(
+            nn.UpsamplingNearest2d(scale_factor=2),
+            nn.Conv2d(in_channels=96, out_channels=32, kernel_size=(5,5), padding='same'),
+            nn.LeakyReLU(True),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3,3), padding='same'),
+            nn.LeakyReLU(True),
+            nn.BatchNorm2d(32)
+        )
 
-        self.conv2 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(5,5))
-        self.conv2_2 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3,3))
+        self.conv6 = nn.Conv2d(in_channels=32, out_channels=8, kernel_size=(5,5), padding='same')
+        self.conv6_2 = nn.Conv2d(in_channels=8, out_channels=8, kernel_size=(3,3), padding='same')
 
-        self.conv3 = nn.Conv2d(in_channels=128, out_channels=96, kernel_size=(5,5))
-        self.conv3_2 = nn.Conv2d(in_channels=96, out_channels=96, kernel_size=(3,3))
-
-        self.conv4 = nn.Conv2d(in_channels=96, out_channels=96, kernel_size=(5,5))
-        self.conv4_2 = nn.Conv2d(in_channels=96, out_channels=96, kernel_size=(3,3))
-
-        self.conv5 = nn.Conv2d(in_channels=96, out_channels=32, kernel_size=(5,5))
-        self.conv5_2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3,3))
-
-        self.conv6 = nn.Conv2d(in_channels=32, out_channels=8, kernel_size=(5,5))
-        self.conv6_2 = nn.Conv2d(in_channels=8, out_channels=8, kernel_size=(3,3))
-
-        self.conv7 = nn.Conv2d(in_channels=8, out_channels=3, kernel_size=(3,3))
+        self.conv7 = nn.Conv2d(in_channels=8, out_channels=3, kernel_size=(3,3), padding='same')
     
     def forward(self,identity_input,orientation_input,emotion_input):
         identity_input = self.fc1_id(identity_input)
@@ -67,14 +90,14 @@ class Model(nn.Module):
         emotion_input = self.fc1_em(emotion_input)
         emotion_input = F.leaky_relu(emotion_input, negative_slope=0.3)
 
+        if self.epsilon == 0:
+            noise = torch.from_numpy(np.random.laplace(loc=0,scale=self.sensitivity/self.epsilon, size=(1, 512)))
+            noise = noise.to(identity_input.device)
+            obfuscated_layer = identity_input + noise
+            params = torch.cat((obfuscated_layer,orientation_input,emotion_input),dim=1).double()
+        else:
+            params = torch.cat((identity_input,orientation_input,emotion_input),dim=1).double()
         
-        noise = torch.from_numpy(np.random.laplace(loc=0,scale=self.sensitivity/self.epsilon, size=(1, 512)))
-        noise = noise.to(identity_input.device)
-        
-
-        obfuscated_layer = identity_input + noise
-
-        params = torch.cat((obfuscated_layer,orientation_input,emotion_input),dim=1).double()
         params = self.fc2(params)
         params = F.leaky_relu(params, negative_slope=0.3)
 
@@ -82,87 +105,23 @@ class Model(nn.Module):
         x = F.leaky_relu(x, negative_slope=0.3)
         x = x.reshape(-1,128,5,4)
 
-        ## I is not making the upasmpling in the 2Dimensions
-        x = self.upsamplig2D(x)
+        x = self.upconv1(x)
+        x = self.upconv2(x)
+        x = self.upconv3(x)
+        x = self.upconv4(x)
+        x = self.upconv5(x)
 
-        height, width = get_paddingSizes(x, self.conv1)
-        x = F.pad(x,(width,width,height,height))
-        x = self.conv1(x)
-        x = F.leaky_relu(x, negative_slope=0.3)
-
-        height, width = get_paddingSizes(x, self.conv1_2)
-        x = F.pad(x,(width,width,height,height))
-        x = self.conv1_2(x)
-        x = F.leaky_relu(x, negative_slope=0.3)
-
-        x = self.batchnorm128(x)
-        x = self.upsamplig2D(x)
-
-        height, width = get_paddingSizes(x, self.conv2)
-        x = F.pad(x,(width,width,height,height))
-        x = self.conv2(x)
-        x = F.leaky_relu(x, negative_slope=0.3)
-
-        height, width = get_paddingSizes(x, self.conv2_2)
-        x = F.pad(x,(width,width,height,height))
-        x = self.conv2_2(x)
-        x = F.leaky_relu(x, negative_slope=0.3)
-
-        x = self.batchnorm128(x)
-        x = self.upsamplig2D(x)
-
-        height, width = get_paddingSizes(x, self.conv3)
-        x = F.pad(x,(width,width,height,height))
-        x = self.conv3(x)
-        x = F.leaky_relu(x, negative_slope=0.3)
-
-        height, width = get_paddingSizes(x, self.conv3_2)
-        x = F.pad(x,(width,width,height,height))
-        x = self.conv3_2(x)
-        x = F.leaky_relu(x, negative_slope=0.3)
-
-        x = self.batchnorm96(x)
-        x = self.upsamplig2D(x)
-
-        height, width = get_paddingSizes(x, self.conv4)
-        x = F.pad(x,(width,width,height,height))
-        x = self.conv4(x)
-        x = F.leaky_relu(x, negative_slope=0.3)
-
-        height, width = get_paddingSizes(x, self.conv4_2)
-        x = F.pad(x,(width,width,height,height))
-        x = self.conv4_2(x)
-        x = F.leaky_relu(x, negative_slope=0.3)
-
-        x =self.batchnorm96(x)
-        x = self.upsamplig2D(x)
-
-        height, width = get_paddingSizes(x, self.conv5)
-        x = F.pad(x,(width,width,height,height))
-        x = self.conv5(x)
-        x = F.leaky_relu(x, negative_slope=0.3)
-
-        height, width = get_paddingSizes(x, self.conv5_2)
-        x = F.pad(x,(width,width,height,height))
-        x = self.conv5_2(x)
-        x = F.leaky_relu(x, negative_slope=0.3)
-
-        x = self.batchnorm32(x)
+        
         x = F.max_pool2d(x,kernel_size=1)
         x = self.upsamplig2D(x)
 
-        height, width = get_paddingSizes(x, self.conv6)
-        x = F.pad(x,(width,width,height,height))
         x = self.conv6(x)
         x = F.leaky_relu(x, negative_slope=0.3)
 
-        height, width = get_paddingSizes(x, self.conv6_2)
-        x = F.pad(x,(width,width,height,height))
         x = self.conv6_2(x)
         x = F.leaky_relu(x, negative_slope=0.3)
 
-        height, width = get_paddingSizes(x, self.conv7)
-        x = F.pad(x,(width,width,height,height))
+        
         x = self.conv7(x)
         x = torch.sigmoid(x)
 
